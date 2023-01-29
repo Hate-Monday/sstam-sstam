17802ea8caf36ee779c7d822103de37c067aad48
권해원 <qpinky12@naver.com>
2023-01-29 06:35:47 -0800

    INIT:first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { FC } from 'react';
 import { appStore } from './store';
 import {
@@ -9,6 +9,7 @@ import {
   CameraRuntimeError,
 } from 'react-native-vision-camera';
 import { PermissionsAndroid } from 'react-native';
+import RNFS, { PicturesDirectoryPath } from 'react-native-fs';
 
 export const Main: FC = () => {
   const { barStyle, backgroundColor } = appStore.useInitTheme();
@@ -18,6 +19,8 @@ export const Main: FC = () => {
   const camera = useRef<Camera>(null);
   const devices = useCameraDevices();
   const device = devices.back;
+  const [showCamera, setShowCamera] = useState(true);
+  const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
     Camera.getCameraPermissionStatus().then(setCameraPermission);
@@ -44,6 +47,20 @@ export const Main: FC = () => {
     }
   };
 
+  const onPressButton = async () => {
+    if (!camera.current) return;
+    const photo = await camera.current.takePhoto({
+      flash: 'on',
+      qualityPrioritization: 'speed',
+    });
+    setPhotos([...photos, photo]);
+    setShowCamera(false);
+    const fileName = photo.path.split('mrousavy')[1];
+    await RNFS.moveFile(`/${photo.path}`, `${RNFS.PicturesDirectoryPath}/${fileName}.jpg`).then(
+      () => console.log('Image Moved', `${fileName}`, '-- to --', `${RNFS.PicturesDirectoryPath}`),
+    );
+  };
+
   useEffect(() => {
     requestCameraPermission();
   }, []);
@@ -58,9 +75,51 @@ export const Main: FC = () => {
 
   return (
     <>
-      {device && (
-        <Camera style={StyleSheet.absoluteFill} isActive photo device={device} ref={camera} />
+      {device && showCamera && (
+        <Camera style={StyleSheet.absoluteFill} isActive video photo device={device} ref={camera} />
+      )}
+      {showCamera && (
+        <View style={styles.area}>
+          <TouchableOpacity style={styles.camButton} onPress={onPressButton}>
+            <Text></Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      {photos && photos.length > 0 && (
+        <View style={{ width: '100%', height: '100%' }}>
+          {photos.map((photo, index) => (
+            <View key={index}>
+              <Image
+                style={{ width: '100%', height: '100%' }}
+                source={{ uri: 'file://' + photo.path }}
+              />
+            </View>
+          ))}
+        </View>
       )}
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+  area: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: '10%',
+  },
+  camButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: 'white',
+    backgroundColor: '#e34077',
+    elevation: 6,
+  },
+});
